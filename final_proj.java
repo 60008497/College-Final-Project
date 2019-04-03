078b6521a9ad39e5e90baa7f69c38842a2e3b5cd
60008497 <43180023+60008497@users.noreply.github.com>
2019-04-03 10:59:36 -0600

    Added .java file
    Main file for program

---
+++ final_proj.java
@@ -0,0 +1,13 @@
+
+
+class final_proj {
+	// For doing the program, randomizing variables, and calling methods.
+	public static void main(String[] args) {
+		
+	}
+	
+	// Displays the 3 door types, based off of input from main method, and asks player to choose a door, returns door properties.
+	public static void doorMain(String[] args) {
+		
+	}
+}
