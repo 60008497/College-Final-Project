5ea1243a85cc73ebfae30943f07789b112ec37a5
60008497 <43180023+60008497@users.noreply.github.com>
2019-04-09 10:02:16 -0600

    Added randomizer
    Added random % generator, and gets Hi, Med, and Low based on %

--- final_proj.java
+++ final_proj.java
@@ -5,11 +5,33 @@ B2
 Project is a randomly generated text-based risk/reward game (A kind of roguelike).
 */
 import java.util.Scanner;
+import java.util.*;
 
 class final_proj {
-	// For doing the program, randomizing variables, and calling methods.
+	// For executing the program, randomizing variables, and calling methods.
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int[] riskInt = new int[3];
+		String[] risk = new String[3];
+		Random rand = new Random();
 		
+		
+		
+		// Get 3 random risk factors
+		for (int i = 0; i <= 2; i++) {
+			riskInt[i] = rand.nextInt(100) + 1;
+			System.out.println(riskInt[i]); 
+			if (riskInt[i] >= 0 && riskInt[i] <= 33) {
+				risk[i] = "Low ";
+			}
+			if (riskInt[i] >= 34 && riskInt[i] <= 66) {
+				risk[i] = "Med ";
+			}
+			if (riskInt[i] >= 67 && riskInt[i] <= 100) {
+				risk[i] = "High";
+			}
+			System.out.println(risk[i]);
+		}
 	}
 	
 	// Displays the 3 door types, based off of input from main method, and asks player to choose a door, returns door properties.
