63bde15d7478ac4f579bafbd009964e11a2aa21b
60008497 <43180023+60008497@users.noreply.github.com>
2019-04-26 10:06:21 -0600

    Updt
    Made the program loop, program accurately displays player properties, now just to display the doors, double random, and make a bunch of outcomes!!

--- final_proj.java
+++ final_proj.java
@@ -11,6 +11,7 @@ class final_proj {
 	// For executing the program, randomizing variables, and calling methods.
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		int playerInput = 0;
 		int[] riskInt = new int[3];
 		String[] risk = new String[3];
 		
@@ -18,26 +19,37 @@ class final_proj {
 		Random rand = new Random();
 		
 		//Player Stats
-		int health = 100
-		int stamina = 10
-		int money = 10
+		int health = 100;
+		int stamina = 10;
+		int money = 10;
 		
-		// Get 3 random risk factors
-		for (int i = 0; i <= 2; i++) {
-			riskInt[i] = rand.nextInt(100) + 1;
-			if (riskInt[i] >= 0 && riskInt[i] <= 33) {
-				risk[i] = "Low ";
+		//The loop of the program!
+		while (health > 0 && stamina > 0) {
+			
+			// Get 3 random risk factors
+			for (int i = 0; i <= 2; i++) {
+				riskInt[i] = rand.nextInt(100) + 1;
+				if (riskInt[i] >= 0 && riskInt[i] <= 33) {
+					risk[i] = "Low ";
+				}
+				if (riskInt[i] >= 34 && riskInt[i] <= 66) {
+					risk[i] = "Med ";
+				}
+				if (riskInt[i] >= 67 && riskInt[i] <= 100) {
+					risk[i] = "High";
+				}
 			}
-			if (riskInt[i] >= 34 && riskInt[i] <= 66) {
-				risk[i] = "Med ";
+			
+			//Clear, then basic displays.
+			for (int n = 0; n <= 100; n++) {
+				System.out.printf("\n");
 			}
-			if (riskInt[i] >= 67 && riskInt[i] <= 100) {
-				risk[i] = "High";
-			}
+			System.out.print("\n ___________________________________________ ");
+			System.out.print("\n| Money: " + String.format("%1$5s", money) + " Stamina: " + String.format("%1$5s", stamina) + " Health: " + String.format("%1$5s", health) + " |");
+			System.out.print("\n|                                           |");
+			
+			health = 0;
+			// stamina = 0;
 		}
-		
-		System.out.printf(" ___________________________________________ ");
-		System.out.printf("| Money:       Stamina:       Health:       |");
-		System.out.printf("| Items:                                    |");
 	}
 }
